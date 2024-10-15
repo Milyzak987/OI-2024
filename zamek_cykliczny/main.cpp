57a39bdf4c16a98c046d7ac79164852cf9d309dd
Milyzak987 <milosz.zakrzewski02@gmail.com>
2024-10-16 00:57:07 +0200

    zamek_cykliczny 15

---
+++ zamek_cykliczny/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+string n;
+int res = 0;
+
+void add_one() {
+    int i = n.length() - 1;
+    while (i >= 0 && n[i] == '9') {
+        n[i] = '0';
+        i--;
+    }
+    if (i >= 0) {
+        n[i]++;
+    } else {
+        n = '1' + n;
+    }
+    res++;
+}
+
+void move() {
+    char x = n[0];
+    n.erase(n.begin());
+    n += x;
+    while (n[0] == '0') {
+        n.erase(n.begin());
+    }
+    res++;
+}
+
+bool allNine() {
+    return (n == string(n.length(), '9'));
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+
+    while (n != "1") {
+        int i = n.length() - 1;
+        int x = n[i] - '0';
+        if(allNine()){
+            add_one();
+            move();
+        }else if (x == 0 || x == 9) {
+            move();
+        } else {
+            while(n[i] != '9'){
+                add_one();
+            }
+        }
+    }
+
+    cout << res;
+
+    return 0;
+}
