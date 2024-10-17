910641737233da8e4948535ce0a10ca7751d764e
Milyzak987 <milosz.zakrzewski02@gmail.com>
2024-10-17 23:19:36 +0200

    zamek cykliczny 35 - all improvements

--- zamek_cykliczny/main.cpp
+++ zamek_cykliczny/main.cpp
@@ -1,36 +1,84 @@
+#include <cmath>
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+struct Result {
+    int positionFromEnd;
+    int consecutiveNines;
+    int dif;
+};
 
 string n;
 int res = 0;
 
-void add_one() {
-    int i = n.length() - 1;
+bool oneZeros() {
+    if (n.length() <= 1) {
+        return false;
+    }
+    int x = n[1], i = 1;
+    while (x == '0') {
+        if (i == n.length() - 1) {
+            return true;
+        }
+        i++;
+        x = n[i];
+    }
+    return false;
+}
+
+Result findNineAndDetails() {
+    int length = n.length();
+    int start = (length > 7) ? length - 7 : 0;
+    int consecutiveNines = 0;
+    int firstNinePos = -1;
+    string numbersAfter = "";
+
+    for (int i = length - 1; i >= start; --i) {
+        if (n[i] == '9') {
+            firstNinePos = i;
+            break;
+        }
+    }
+
+    if (firstNinePos == -1) {
+        return {-1, -1, 0};
+    }
+
+    int i = firstNinePos;
     while (i >= 0 && n[i] == '9') {
-        n[i] = '0';
+        consecutiveNines++;
         i--;
     }
-    if (i >= 0) {
-        n[i]++;
-    } else {
-        n = '1' + n;
+
+    if (firstNinePos < length - 1) {
+        numbersAfter = n.substr(firstNinePos + 1);
     }
-    res++;
+
+    int positionFromEnd = length - firstNinePos;
+
+    int base = 1;
+    for (int i = 0; i < positionFromEnd - 1; i++) {
+        base *= 10;
+    }
+
+    int dif = 0;
+    if (numbersAfter != "") {
+        dif = base - stoi(numbersAfter) - 1;
+    }
+
+    return {positionFromEnd, consecutiveNines, dif};
 }
 
-void move() {
-    char x = n[0];
-    n.erase(n.begin());
-    n += x;
-    while (n[0] == '0') {
-        n.erase(n.begin());
+int endNines() {
+    int i = n.length() - 2;
+    int count = 0;
+    while (i >= 0 && n[i] == '9') {
+        count++;
+        i--;
     }
-    res++;
-}
-
-bool allNine() {
-    return (n == string(n.length(), '9'));
+    return count;
 }
 
 int main() {
@@ -39,22 +87,41 @@ int main() {
 
     cin >> n;
 
-    while (n != "1") {
-        int i = n.length() - 1;
+    if (n == "1") {
+        cout << 0;
+        return 0;
+    }
+    if (n[0] == '1' && oneZeros()) {
+        cout << 1;
+        return 0;
+    }
+
+    Result result = findNineAndDetails();
+    if (result.dif < result.consecutiveNines && result.positionFromEnd != 1 && result.positionFromEnd != -1) {
+        for (int i = n.length() - 1; i > n.length() - result.positionFromEnd; i--) {
+            n[i] = '9';
+        }
+        res += result.dif;
+    }
+
+    int count = endNines();
+    if (n.length() >= 2 && n[n.length() - 1] == '0' && n[n.length() - 2] == '9') {
+        res += count;
+    }
+
+    int l = n.length() - count;
+    int j = n.length() - 1;
+    if (n[j] != '0') {
+        res += 9 - (n[j] - '0');
+    }
+    for (int i = 0; i < l - 1; i++) {
         int x = n[i] - '0';
-        if(allNine()){
-            add_one();
-            move();
-        }else if (x == 0 || x == 9) {
-            move();
-        } else {
-            while(n[i] != '9'){
-                add_one();
-            }
+        if (n[i] != '0') {
+            res += 10 - x;
         }
     }
 
-    cout << res;
+    cout << res + 2;
 
     return 0;
 }
