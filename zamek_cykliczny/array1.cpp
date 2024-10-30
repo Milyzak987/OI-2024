3dde2e6d8e178afcc906774a1cdbf3cef02c1b97
“Milyzak987” <milosz.zakrzewski02@gmail.com>
2024-10-30 19:39:15 +0100

    nie dziala btw

--- zamek_cykliczny/array1.cpp
+++ zamek_cykliczny/array1.cpp
@@ -1,13 +1,13 @@
 // array
-#include <cmath>
 #include <iostream>
 #include <string>
 using namespace std;
 
 const int MAXN = 1e6 + 7;
+const int INF = 1e9;
 int lock[MAXN];
 
-bool oneZeros(int n, int res) {
+bool oneZeros(int n, int &res) {
     if (lock[0] == 1) {
         if (n == 1) {
             return true;
@@ -23,8 +23,8 @@ bool oneZeros(int n, int res) {
     return false;
 }
 
-int endNines(int n) {
-    int i = n - 2;
+int endNines(int n, int lim) {
+    int i = n - lim;
     int count = 0;
     while (i >= 0 && lock[i] == 9) {
         count++;
@@ -33,31 +33,34 @@ int endNines(int n) {
     return count;
 }
 
-void nineCase(int n) {
-    for (int t = 1; t <= 8; t++) {
-        long long start = max(n - t, 0);
+void nineCase(int n, int &res, int &minLim) {
+    int minDif = INF;
+    for (int t = 2; t <= 8; t++) {
+        long long start = max(n - 8, 0);
 
         int j = start - 1, nines = 0;
-        while (j >= 0 && lock[j] == 9){
+        while (j >= 0 && lock[j] == 9) {
             j--;
             nines++;
         }
 
-        int dif = 0, base = 1;
+        int moveDif = 0, base = 1, addDif = 0;
         for (long long i = n - 1; i >= start; i--) {
+            if (lock[i] != 0) {
+                addDif += 9 - lock[i];
+            }
+            moveDif += (9 - lock[i]) * base;
             base *= 10;
-            dif += ((9 - lock[i]) * base) - (9 - lock[i]);
         }
-
-        if (dif <= nines) {
-            res += moveDif;
-            for (long long i = 0; i < positionFromEnd; i++) {
-                q.pop_back();
-            }
-            for (long long i = 0; i < positionFromEnd; i++) {
-                q.push_back(9);
-            }
+        int fullDif = moveDif - addDif;
+        // cout << nines << " " << moveDif << "\n";
+        if (fullDif <= nines && moveDif < minDif) {
+            minDif = moveDif;
+            minLim = t + 1;
         }
+    }
+    if (minDif != INF) {
+        res += minDif;
     }
 }
 
@@ -73,22 +76,26 @@ int main() {
         lock[i] = s[i] - '0';
     }
 
-    int res = 0;
+    int res = 0, minLim = 2;
     if (oneZeros(n, res)) {
         cout << res;
         return 0;
     }
 
-    int count = endNines(n);
-    if (lock[n - 1] == '0') {
+    nineCase(n, res, minLim);
+
+    int count = endNines(n, minLim);
+    // cout << count << " " << res << "\n";
+    if (lock[n - 1] == 0 && res == 0) {
         res += count;
     }
 
-    if (lock[n - 1] != '0') {
+    if (lock[n - 1] != 0 && res == 0) {
         res += 9 - lock[n - 1];
     }
 
-    int r = s.length() - count - 1;
+    count += minLim - 1;
+    int r = n - count;
     for (int i = 0; i < r; i++) {
         if (lock[i] != 0) {
             res += 10 - lock[i];
