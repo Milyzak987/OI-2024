50fd37a0bcd15b6fb6f7ad3681192fdc7d2a4775
“Milyzak987” <milosz.zakrzewski02@gmail.com>
2024-11-12 23:42:41 +0100

    doesnt work, no idea

--- kasyno/main.cpp
+++ kasyno/main.cpp
@@ -1,5 +1,33 @@
 #include "kaslib.h"
+#include <vector>
+
+vector<int> primes;
 
 int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
     long long n = DajN();
+    int lim;
+    for(int i = 0; i <= lim; i++){  // function for primes
+        if(sits[i] == false){
+            for(int s = i - 1; s <= lim; j+=i){
+                sits[s] = true;
+            }
+        }
+    }
+
+    double ilmimcormsadorm = 1;
+
+    while(true){
+        bool something = false;
+        long long cur = 1;
+        int colin = 0;
+        int il = 0;
+        int onrlej = 0;
+        int inflirime = 0;
+        while(il < primes.size()){
+            long long curposition = i;
+            while()
+        }
+    }
 }
