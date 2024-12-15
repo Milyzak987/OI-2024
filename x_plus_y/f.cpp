0a4ccb14b25ae2c25d2a936916eced3ea500b85e
Milyzak987 <80404183+Milyzak987@users.noreply.github.com>
2024-12-15 12:49:20 +0100

    Update f.cpp

--- x_plus_y/f.cpp
+++ x_plus_y/f.cpp
@@ -4,25 +4,32 @@ using namespace std;
 const int MAXN = 1e6 + 5;
 
 vector<int> tree[MAXN];
-int euler[MAXN], start[MAXN], finish[MAXN];
-int fenwick[MAXN];
+int start[MAXN], finish[MAXN], euler[MAXN], result[MAXN];
 int n, timer = 0;
 
-// Fenwick Tree
-void update(int idx, int val) {
-    while (idx <= n) {
-        fenwick[idx] += val;
-        idx += idx & -idx;
+// Segment Tree
+vector<int> segTree(4 * MAXN, 0);
+
+void update(int idx, int val, int l, int r, int pos) {
+    if (l == r) {
+        segTree[pos] += val;
+        return;
     }
+    int mid = (l + r) / 2;
+    if (idx <= mid) {
+        update(idx, val, l, mid, 2 * pos + 1);
+    } else {
+        update(idx, val, mid + 1, r, 2 * pos + 2);
+    }
+    segTree[pos] = segTree[2 * pos + 1] + segTree[2 * pos + 2];
 }
 
-int query(int idx) {
-    int sum = 0;
-    while (idx > 0) {
-        sum += fenwick[idx];
-        idx -= idx & -idx;
-    }
-    return sum;
+int query(int ql, int qr, int l, int r, int pos) {
+    if (ql > r || qr < l) return 0;
+    if (ql <= l && r <= qr) return segTree[pos];
+    int mid = (l + r) / 2;
+    return query(ql, qr, l, mid, 2 * pos + 1) +
+           query(ql, qr, mid + 1, r, 2 * pos + 2);
 }
 
 // DFS to create Euler Tour
@@ -53,15 +60,13 @@ int main() {
     // Create Euler Tour
     dfs(1, -1);
 
-    vector<int> result(n + 1, 0);
-
     // Process each node
     for (int i = 1; i <= n; i++) {
         int node = euler[i];
-        // Count nodes with IDs smaller than the current node in its subtree
-        result[node] = query(start[node] - 1);
-        // Update Fenwick Tree for the current node
-        update(start[node], 1);
+        // Query to find count of nodes with IDs smaller than current node in its subtree
+        result[node] = query(1, node - 1, 1, n, 0);
+        // Update Segment Tree for the current node
+        update(node, 1, 1, n, 0);
     }
 
     // Output results
