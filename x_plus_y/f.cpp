8e5adc13eb9f252cc13152e9d7ff1062bcd59ed0
Milyzak987 <80404183+Milyzak987@users.noreply.github.com>
2024-12-15 10:28:11 +0100

    Update f.cpp

--- x_plus_y/f.cpp
+++ x_plus_y/f.cpp
@@ -1,65 +1,82 @@
-#include <iostream>
-#include <vector>
-#include <stack>
-#include <algorithm>
-
+// C++ program to find the maximum area of 
+// rectangle in a 2D matrix.
+#include <bits/stdc++.h>
 using namespace std;
 
-// Funkcja pomocnicza do obliczenia największego prostokąta w histogramie
-int largestRectangleInHistogram(const vector<int>& heights) {
+// Function to find the maximum area of 
+// rectangle in a histogram.
+int getMaxArea(vector<int>& arr) {
+    int n = arr.size();
     stack<int> s;
-    int maxArea = 0;
-    int n = heights.size();
-
-    for (int i = 0; i <= n; ++i) {
-        int h = (i < n) ? heights[i] : 0; // Dodajemy "0" na końcu histogramu
-        while (!s.empty() && heights[s.top()] >= h) {
-            int height = heights[s.top()];
+    int res = 0;
+    int tp, curr;
+    for (int i = 0; i < n; i++) {      
+         
+        while (!s.empty() && arr[s.top()] >= arr[i]) {
+          
+            // The popped item is to be considered as the 
+            // smallest element of the histogram
+            tp = s.top(); 
             s.pop();
+          
+            // For the popped item previous smaller element is 
+            // just below it in the stack (or current stack top)
+            // and next smaller element is i
             int width = s.empty() ? i : i - s.top() - 1;
-            maxArea = max(maxArea, height * width);
+          
+            res = max(res,  arr[tp] * width);
         }
         s.push(i);
     }
 
-    return maxArea;
-}
-
-// Funkcja główna do znajdowania największego prostokąta w macierzy
-int largestRectangleInMatrix(const vector<vector<int>>& matrix) {
-    if (matrix.empty()) return 0;
-
-    int n = matrix.size();
-    int m = matrix[0].size();
-    vector<int> heights(m, 0);
-    int maxArea = 0;
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            if (matrix[i][j] == 0) {
-                heights[j] += 1;
-            } else {
-                heights[j] = 0;
-            }
-        }
-        maxArea = max(maxArea, largestRectangleInHistogram(heights));
+    // For the remaining items in the stack, next smaller does
+    // not exist. Previous smaller is the item just below in
+    // stack.
+    while (!s.empty()) {
+        tp = s.top(); s.pop();
+        curr = arr[tp] * (s.empty() ? n : n - s.top() - 1);
+        res = max(res, curr);
     }
 
-    return maxArea;
+    return res;
+}
+
+// Function to find the maximum area of rectangle
+// in a 2D matrix.
+int maxArea(vector<vector<int>> &mat) {
+    int n = mat.size(), m = mat[0].size();
+    
+    // Array to store matrix 
+    // as a histogram.
+    vector<int> arr(m, 0);
+    
+    int ans = 0;
+    
+    // Traverse row by row.
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            if (mat[i][j]==1) {
+                arr[j]++;
+            }
+            else {
+                arr[j] = 0;
+            }
+        }
+        
+        ans = max(ans, getMaxArea(arr));
+    }
+    
+    return ans;
 }
 
 int main() {
-    int n;
-    cin >> n;
-
-    vector<vector<int>> matrix(n, vector<int>(n));
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            cin >> matrix[i][j];
-        }
-    }
-
-    cout << largestRectangleInMatrix(matrix) << endl;
-
+    vector<vector<int>> mat = 
+        {{0,1,1,0},
+         {1,1,1,1},
+         {1,1,1,1},
+         {1,1,0,0}};
+         
+    cout << maxArea(mat) << endl;
+    
     return 0;
 }
