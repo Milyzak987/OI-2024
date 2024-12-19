4e361f55e56d6e841cd2c1c025c06a2cc06ab0ae
Milyzak987 <80404183+Milyzak987@users.noreply.github.com>
2024-12-19 09:45:42 +0100

    Update f.cpp

--- x_plus_y/f.cpp
+++ x_plus_y/f.cpp
@@ -3,76 +3,67 @@
 #include <unordered_map>
 using namespace std;
 
-class SegmentTree {
-    vector<unordered_map<int, int>> tree;
-    int n;
+// Global variables
+vector<unordered_map<int, int>> tree;
+int n;
 
-    void build(vector<int>& arr, int v, int tl, int tr) {
-        if (tl == tr) {
-            tree[v][arr[tl]]++;
-        } else {
-            int tm = (tl + tr) / 2;
-            build(arr, v * 2, tl, tm);
-            build(arr, v * 2 + 1, tm + 1, tr);
-            merge(tree[v * 2], tree[v * 2 + 1], tree[v]);
-        }
-    }
-
-    void update(int v, int tl, int tr, int pos, int oldVal, int newVal) {
-        tree[v][oldVal]--;
-        if (tree[v][oldVal] == 0) {
-            tree[v].erase(oldVal);
-        }
-        tree[v][newVal]++;
-        if (tl != tr) {
-            int tm = (tl + tr) / 2;
-            if (pos <= tm)
-                update(v * 2, tl, tm, pos, oldVal, newVal);
-            else
-                update(v * 2 + 1, tm + 1, tr, pos, oldVal, newVal);
-        }
-    }
-
-    int query(int v, int tl, int tr, int l, int r, int x) {
-        if (l > r) return 0;
-        if (l == tl && r == tr) {
-            return tree[v].count(x) ? tree[v][x] : 0;
-        }
+// Function to build the segment tree
+void build(vector<int>& arr, int v, int tl, int tr) {
+    if (tl == tr) {
+        tree[v][arr[tl]]++;
+    } else {
         int tm = (tl + tr) / 2;
-        return query(v * 2, tl, tm, l, min(r, tm), x) +
-               query(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r, x);
-    }
-
-    void merge(const unordered_map<int, int>& left, const unordered_map<int, int>& right, unordered_map<int, int>& result) {
-        result = left;
-        for (const auto& [key, value] : right) {
-            result[key] += value;
+        build(arr, v * 2, tl, tm);
+        build(arr, v * 2 + 1, tm + 1, tr);
+        for (const auto& [key, value] : tree[v * 2]) {
+            tree[v][key] += value;
+        }
+        for (const auto& [key, value] : tree[v * 2 + 1]) {
+            tree[v][key] += value;
         }
     }
+}
 
-public:
-    SegmentTree(vector<int>& arr) {
-        n = arr.size();
-        tree.resize(4 * n);
-        build(arr, 1, 0, n - 1);
+// Function to update the segment tree
+void update(int v, int tl, int tr, int pos, int oldVal, int newVal) {
+    // Update the current node
+    tree[v][oldVal]--;
+    if (tree[v][oldVal] == 0) {
+        tree[v].erase(oldVal);
     }
+    tree[v][newVal]++;
+    if (tl != tr) {
+        int tm = (tl + tr) / 2;
+        if (pos <= tm) {
+            update(v * 2, tl, tm, pos, oldVal, newVal);
+        } else {
+            update(v * 2 + 1, tm + 1, tr, pos, oldVal, newVal);
+        }
+    }
+}
 
-    void update(int pos, int oldVal, int newVal) {
-        update(1, 0, n - 1, pos, oldVal, newVal);
+// Function to query the segment tree
+int query(int v, int tl, int tr, int l, int r, int x) {
+    if (l > r) return 0;
+    if (l == tl && r == tr) {
+        return tree[v].count(x) ? tree[v][x] : 0;
     }
-
-    int query(int l, int r, int x) {
-        return query(1, 0, n - 1, l, r, x);
-    }
-};
+    int tm = (tl + tr) / 2;
+    return query(v * 2, tl, tm, l, min(r, tm), x) +
+           query(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r, x);
+}
 
 int main() {
     vector<int> arr = {1, 2, 1, 3, 2, 1};
-    SegmentTree st(arr);
+    n = arr.size();
+    tree.resize(4 * n);
 
-    // Przykładowe zapytania
-    cout << st.query(0, 5, 1) << endl; // Liczba "1" w całym przedziale
-    st.update(2, 1, 4);                // Zmieniamy arr[2] z 1 na 4
-    cout << st.query(0, 5, 1) << endl; // Liczba "1" w całym przedziale po zmianie
-    cout << st.query(1, 3, 2) << endl; // Liczba "2" w przedziale [1, 3]
+    // Build the segment tree
+    build(arr, 1, 0, n - 1);
+
+    // Example queries and updates
+    cout << query(1, 0, n - 1, 0, 5, 1) << endl; // Count of "1" in the range [0, 5]
+    update(1, 0, n - 1, 2, 1, 4);               // Change arr[2] from 1 to 4
+    cout << query(1, 0, n - 1, 0, 5, 1) << endl; // Count of "1" in the range [0, 5] after update
+    cout << query(1, 0, n - 1, 1, 3, 2) << endl; // Count of "2" in the range [1, 3]
 }
