cb2dc65aee68cc902017771c9e4e1ae62ed90918
Milyzak987 <80404183+Milyzak987@users.noreply.github.com>
2025-01-10 08:53:08 +0100

    Update f.cpp

--- x_plus_y/f.cpp
+++ x_plus_y/f.cpp
@@ -4,54 +4,49 @@ typedef long long ll;
 typedef long double ld;
 typedef pair<int, int> para;
 
-const int MAX = 1000 * 1000;
-int A[MAX + 1];
-int B[MAX + 1];
-int schematy[MAX + 1];
-int dp[MAX + 1];
+const int MAX = 200 * 1000;
+const ll MOD = 1000 * 1000 * 1000 + 7;
+const ll P = 29;
+ll potegi_P[MAX + 1];
+ll pref[MAX + 1];
+vector<int> wynik;
+
+ll suma_pref(ll pocz, ll kon) {
+	return (pref[kon] - (pref[pocz - 1] * potegi_P[kon - pocz + 1] % MOD) + MOD) % MOD;
+} 
 
 int main() {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
+	potegi_P[0] = 1;
+	for (int i = 1; i <= MAX; i++) {
+		potegi_P[i] = potegi_P[i - 1] * P % MOD;
+	}
 
-        int n, m;
-        cin >> n >> m;
+	string a, b;
+	cin >> a >> b;
+	int n = (int) a.length(), m = (int) b.length();
+	a = "$" + a;
+	b = "$" + b;
+	
+	ll wzor = 0;
+	for (int i = 1; i <= n; i++) {
+		wzor = (wzor * P + a[i] - 'a') % MOD;
+	}
+	
+	for (int i = 1; i <= m; i++) {
+		pref[i] = (pref[i - 1] * P + b[i] - 'a') % MOD;
+	}
+	
+	for (int i = 1; i + n - 1 <= m; i++) {
+		if (suma_pref(i, i + n - 1) == wzor) {
+			wynik.push_back(i);
+		}
+	}
+	
+	cout << wynik.size() << "\n";
+	for (int i : wynik) cout << i << " ";
 
-        for (int i = 1; i <= n; i++) {
-                cin >> A[i];
-        }
-        for (int i = 1; i <= n; i++) {
-                cin >> B[i];
-        }
-
-        for (int i = 0; i <= MAX; i++) schematy[i] = INT_MAX;
-        for (int i = 1; i <= n; i++) {
-                schematy[A[i]] = min(schematy[A[i]], A[i] - B[i]);
-        }
-        for (int i = 1; i <= MAX; i++) {
-                schematy[i] = min(schematy[i], schematy[i - 1]);
-        }
-
-        for (int i = 1; i <= MAX; i++) {
-                if (schematy[i] != INT_MAX) {
-                        dp[i] = dp[max(0, i - schematy[i])] + 2;
-                }
-        }
-
-        ll wynik = 0;
-        for (int i = 1; i <= m; i++) {
-                int masa;
-                cin >> masa;
-
-                while (masa > MAX) {
-                        int ile = (masa - MAX - 1) / schematy[MAX] + 1;
-                        masa -= ile * schematy[MAX];
-                        wynik += 2 * ile;
-                }
-                wynik += dp[masa];
-        }
-        cout << wynik;
-
-        return 0;
+	return 0;
 }
-
