6fc9554b1a6c796318f4391b28a535311c861c0a
Milyzak987 <80404183+Milyzak987@users.noreply.github.com>
2025-01-25 10:42:57 +0100

    Update f.cpp

--- x_plus_y/f.cpp
+++ x_plus_y/f.cpp
@@ -1,52 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef long double ld;
-typedef pair<int, int> para;
+using ll = long long;
 
-const int MAX = 200 * 1000;
-const ll MOD = 1000 * 1000 * 1000 + 7;
-const ll P = 29;
-ll potegi_P[MAX + 1];
-ll pref[MAX + 1];
-vector<int> wynik;
+const int MAXN = 2e5 + 7;
 
-ll suma_pref(ll pocz, ll kon) {
-	return (pref[kon] - (pref[pocz - 1] * potegi_P[kon - pocz + 1] % MOD) + MOD) % MOD;
-} 
+void solve() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int q;
+    cin >> q;
+
+    while (q--) {
+        ll l, r;
+        cin >> l >> r;
+
+        if (l <= 0 && r >= 0) {
+            cout << -1 << '\n'; // Infinite solutions
+            continue;
+        }
+
+        ll result = 0;
+        for (ll s = l; s <= r; ++s) {
+            // Count pairs (x1, x2) such that x1 + x2 = -b and x1 * x2 = c
+            // and b + c = s
+            // Rearrange: b = s - c
+            for (ll x1 = 1; x1 * x1 <= abs(s); ++x1) {
+                if (s % x1 == 0) {
+                    ll x2 = s / x1;
+                    if (x1 <= x2) result++;
+                }
+            }
+        }
+
+        cout << result << '\n';
+    }
+}
 
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	
-	potegi_P[0] = 1;
-	for (int i = 1; i <= MAX; i++) {
-		potegi_P[i] = potegi_P[i - 1] * P % MOD;
-	}
-
-	string a, b;
-	cin >> a >> b;
-	int n = (int) a.length(), m = (int) b.length();
-	a = "$" + a;
-	b = "$" + b;
-	
-	ll wzor = 0;
-	for (int i = 1; i <= n; i++) {
-		wzor = (wzor * P + a[i] - 'a') % MOD;
-	}
-	
-	for (int i = 1; i <= m; i++) {
-		pref[i] = (pref[i - 1] * P + b[i] - 'a') % MOD;
-	}
-	
-	for (int i = 1; i + n - 1 <= m; i++) {
-		if (suma_pref(i, i + n - 1) == wzor) {
-			wynik.push_back(i);
-		}
-	}
-	
-	cout << wynik.size() << "\n";
-	for (int i : wynik) cout << i << " ";
-
-	return 0;
+    solve();
+    return 0;
 }
