2434f0a234ef8236a7334d238e2d1e2dcefabe96
Milyzak987 <80404183+Milyzak987@users.noreply.github.com>
2025-01-30 11:49:34 +0100

    Update f.cpp

--- x_plus_y/f.cpp
+++ x_plus_y/f.cpp
@@ -1,71 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-ios_base::sync_with_stdio(0);
-cin.tie(0);
-
-const int MAXN = 2e5 + 7;
-vector<int> graph[MAXN];
-
 struct Group {
-    int id, size;
-    bool can_split;
+    int id;     // Numer grupy
+    int size;   // Liczba osób w grupie
+    bool canSplit; // Czy mogą się podzielić
 };
 
-queue<Group> q;
-unordered_map<int, Group> active_groups;
-int group_counter = 0;
-
 int main() {
-    int query_count;
-    cin >> query_count;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    while (query_count--) {
+    int q;
+    cin >> q;
+
+    deque<Group> queue;  // Kolejka grup
+    unordered_map<int, bool> removed; // Lazy deletion dla usuwania grup
+    int groupId = 0; // Numerowanie grup
+
+    while (q--) {
         char type;
         cin >> type;
 
         if (type == 'D') {
-            int size, can_split;
-            cin >> size >> can_split;
-            ++group_counter;
-            Group new_group = {group_counter, size, can_split};
-            q.push(new_group);
-            active_groups[group_counter] = new_group;
+            int z, c;
+            cin >> z >> c;
+            queue.push_back({++groupId, z, c == 1});
+            removed[groupId] = false;  // Grupa jest aktywna
         } 
         else if (type == 'R') {
-            int id;
-            cin >> id;
-            active_groups.erase(id);
+            int i;
+            cin >> i;
+            removed[i] = true;  // Oznaczamy grupę jako usuniętą (lazy deletion)
         } 
         else if (type == 'P') {
-            long long seats;
-            cin >> seats;
-            
-            vector<pair<int, int>> result;
-            
-            while (!q.empty() && seats > 0) {
-                Group current = q.front();
-                if (active_groups.find(current.id) == active_groups.end()) {
-                    q.pop();
-                    continue;
-                }
+            int m;
+            cin >> m;
 
-                if (current.size <= seats) {
-                    result.emplace_back(current.id, current.size);
-                    seats -= current.size;
-                    q.pop();
-                    active_groups.erase(current.id);
-                } 
-                else if (current.can_split) {
-                    result.emplace_back(current.id, seats);
-                    active_groups[current.id].size -= seats;
-                    seats = 0;
-                } 
-                else {
+            vector<pair<int, int>> result;  // Lista grup w pontonie
+            while (m > 0 && !queue.empty()) {
+                Group front = queue.front();
+                queue.pop_front();
+
+                if (removed[front.id]) continue; // Lazy deletion - pomijamy usunięte grupy
+
+                if (front.size <= m) {  // Cała grupa się mieści
+                    result.push_back({front.id, front.size});
+                    m -= front.size;
+                } else if (front.canSplit) {  // Grupa się dzieli
+                    result.push_back({front.id, m});
+                    queue.push_front({front.id, front.size - m, front.canSplit});
+                    m = 0;
+                } else {  // Grupa nie może się podzielić, zostaje w kolejce
+                    queue.push_front(front);
                     break;
                 }
             }
 
+            // Wypisujemy wynik
             cout << result.size() << '\n';
             for (auto &[id, count] : result) {
                 cout << id << ' ' << count << '\n';
