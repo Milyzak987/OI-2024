f8b54718efd403c5da8f1fc3908ad58731742612
Milyzak987 <80404183+Milyzak987@users.noreply.github.com>
2025-01-30 09:39:35 +0100

    Update f.cpp

--- x_plus_y/f.cpp
+++ x_plus_y/f.cpp
@@ -1,101 +1,65 @@
-#include "szylib.h"
-#include <vector>
-#include <string>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-// Funkcja kodowania wiadomości
-void Zaszyfruj(int X, int Y, int N, string S) {
-    vector<vector<int>> T(8, vector<int>(8, 0)); // Tablica do przechowywania bitów
-    int row = 0, col = 0;
+const int MAXN = 2e5 + 7;
+vector<int> graph[MAXN], rev_graph[MAXN];
+int in_degree[MAXN], out_degree[MAXN];
+int result[MAXN];
 
-    // Pomocnicza funkcja do znalezienia następnego wolnego pola (omija X i Y)
-    auto next_valid = [&]() {
-        do {
-            if (++col == 8) {
-                col = 0;
-                ++row;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> match_order(n + 1, -1);
+    
+    for (int i = 0; i < m; i++) {
+        int x, y;
+        cin >> x >> y;
+        graph[y].push_back(x);
+        rev_graph[x].push_back(y);
+        in_degree[x]++;
+        out_degree[y]++;
+    }
+
+    queue<int> q;
+    vector<int> topo;
+    
+    for (int i = 1; i <= n; i++) {
+        if (in_degree[i] == 0) {
+            q.push(i);
+            match_order[i] = 0;
+        }
+    }
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        topo.push_back(node);
+        
+        for (int v : graph[node]) {
+            in_degree[v]--;
+            if (in_degree[v] == 0) {
+                q.push(v);
+                match_order[v] = match_order[node] + 1;
             }
-        } while (row == X || col == Y);
-    };
-
-    // Zakodowanie wiadomości
-    for (int i = 0; i < N; ++i) {
-        T[row][col] = (S[i] == 'B' ? 1 : 0);
-        next_valid();
-    }
-
-    // Obliczenie sum kontrolnych
-    vector<int> row_parity(8, 0), col_parity(8, 0);
-    for (int i = 0; i < 8; ++i) {
-        for (int j = 0; j < 8; ++j) {
-            row_parity[i] ^= T[i][j];
-            col_parity[j] ^= T[i][j];
         }
     }
 
-    // Wpisanie bitów parzystości do uszkodzonych miejsc
-    T[X][Y] = 0; // Możemy wpisać cokolwiek, bo to i tak się zepsuje
-    for (int i = 0; i < 8; ++i) {
-        if (i != X) T[i][Y] = row_parity[i]; // Parzystość wiersza
-        if (i != Y) T[X][i] = col_parity[i]; // Parzystość kolumny
-    }
-
-    // Ustawienie kryształów
-    for (int i = 0; i < 8; ++i) {
-        for (int j = 0; j < 8; ++j) {
-            if (i != X && j != Y) {
-                Oznacz(i, j, T[i][j]);
-            }
-        }
-    }
-}
-
-// Funkcja dekodowania wiadomości
-string Odszyfruj(int N, vector<vector<int>> T) {
-    int X = -1, Y = -1;
-
-    // Znalezienie X i Y przez analizę sum kontrolnych
-    vector<int> row_parity(8, 0), col_parity(8, 0);
-    for (int i = 0; i < 8; ++i) {
-        for (int j = 0; j < 8; ++j) {
-            row_parity[i] ^= T[i][j];
-            col_parity[j] ^= T[i][j];
+    if ((int)topo.size() < n) {
+        fill(result, result + n + 1, -1);
+    } else {
+        for (int i = 1; i <= n; i++) {
+            result[i] = match_order[i];
         }
     }
 
-    // Uszkodzony wiersz X → jedyny, gdzie suma parzystości się nie zgadza
-    for (int i = 0; i < 8; ++i) {
-        if (row_parity[i] != 0) {
-            X = i;
-            break;
-        }
+    for (int i = 1; i <= n; i++) {
+        cout << result[i] << " ";
     }
+    cout << "\n";
 
-    // Uszkodzona kolumna Y → jedyna, gdzie suma parzystości się nie zgadza
-    for (int j = 0; j < 8; ++j) {
-        if (col_parity[j] != 0) {
-            Y = j;
-            break;
-        }
-    }
-
-    // Odczytanie wiadomości
-    string result;
-    int row = 0, col = 0;
-    auto next_valid = [&]() {
-        do {
-            if (++col == 8) {
-                col = 0;
-                ++row;
-            }
-        } while (row == X || col == Y);
-    };
-
-    for (int i = 0; i < N; ++i) {
-        result += (T[row][col] == 1 ? 'B' : 'A');
-        next_valid();
-    }
-
-    return result;
+    return 0;
 }
