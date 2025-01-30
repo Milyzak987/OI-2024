547a0c4f4a307c747c3701cb0e26946062c1da10
Milyzak987 <80404183+Milyzak987@users.noreply.github.com>
2025-01-30 09:45:24 +0100

    Update f.cpp

--- x_plus_y/f.cpp
+++ x_plus_y/f.cpp
@@ -1,61 +1,69 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+
 using namespace std;
 
-const int MAXN = 2e5 + 7;
-vector<int> graph[MAXN], rev_graph[MAXN];
-int in_degree[MAXN], out_degree[MAXN];
-int result[MAXN];
+const int MAX_N = 200000;
+vector<int> graph[MAX_N + 1];
+int in_degree[MAX_N + 1];
+int result[MAX_N + 1];
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
     int n, m;
     cin >> n >> m;
 
-    vector<int> match_order(n + 1, -1);
-    
+    // Wczytanie meczów i budowanie grafu
     for (int i = 0; i < m; i++) {
         int x, y;
         cin >> x >> y;
         graph[y].push_back(x);
-        rev_graph[x].push_back(y);
         in_degree[x]++;
-        out_degree[y]++;
     }
 
+    // Kolejka dla sortowania topologicznego (Kahn's Algorithm)
     queue<int> q;
-    vector<int> topo;
-    
     for (int i = 1; i <= n; i++) {
         if (in_degree[i] == 0) {
             q.push(i);
-            match_order[i] = 0;
         }
     }
 
+    int match_count = 0;
+    int rank = n;
     while (!q.empty()) {
-        int node = q.front();
-        q.pop();
-        topo.push_back(node);
+        match_count++;
+        int size = q.size();
         
-        for (int v : graph[node]) {
-            in_degree[v]--;
-            if (in_degree[v] == 0) {
-                q.push(v);
-                match_order[v] = match_order[node] + 1;
+        vector<int> current_nodes;
+        for (int i = 0; i < size; i++) {
+            int node = q.front();
+            q.pop();
+            current_nodes.push_back(node);
+        }
+        
+        for (int node : current_nodes) {
+            result[node] = match_count;
+            for (int neighbor : graph[node]) {
+                in_degree[neighbor]--;
+                if (in_degree[neighbor] == 0) {
+                    q.push(neighbor);
+                }
             }
         }
     }
 
-    if ((int)topo.size() < n) {
-        fill(result, result + n + 1, -1);
-    } else {
-        for (int i = 1; i <= n; i++) {
-            result[i] = match_order[i];
+    // Sprawdzenie nieokreślonych rang
+    for (int i = 1; i <= n; i++) {
+        if (in_degree[i] > 0) {
+            result[i] = -1;
         }
     }
 
+    // Wypisanie wyniku
     for (int i = 1; i <= n; i++) {
         cout << result[i] << " ";
     }
